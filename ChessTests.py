cf3a9d123c7ab397d2de2f627d9cdaa2234e485e
elmartirta <elmartirta@rogers.com>
2020-03-20 11:35:38 -0400

    Write program testing boilerplate code
    This file automatically tests Chess.py under the
    FIDE rules of chess, making sure that it runs
    correctly.

---
+++ ChessTests.py
@@ -0,0 +1,25 @@
+from Chess import *
+
+"""
+These tests check whether the implementation of
+Chess.py correctly follows the FIDE rules of chess.
+"""
+
+
+
+def main():
+    print("===================")
+    print("=== Chess Tests ===")
+    print("===================")
+    print("")
+    
+    #Tests go here
+
+    print("")
+    print("-----------------------------------------")
+    print("Test Run Complete, press any key to exit.")
+    print("-----------------------------------------")
+
+
+if (__name__ == "__main__"):
+    main()
