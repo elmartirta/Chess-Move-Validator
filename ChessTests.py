0859c2fbffc98816740a2963c4bad7dbfbccb669
elmartirta <elmartirta@rogers.com>
2020-03-20 11:55:53 -0400

    Unimplement Article 1.1
    Article 1 is not a technical description of the laws of chess,
    but is instead a general, qualitative description of the rules
    of chess. As such, it might not be possible to test Article
    1 without implementing tests for all of the other articles
    as a side effect.

--- ChessTests.py
+++ ChessTests.py
@@ -5,37 +5,11 @@ These tests check whether the implementation of
 Chess.py correctly follows the FIDE rules of chess.
 """
 
-"""
-[Article 1.1] - What is Chess
-The game of chess is played between two opponents who move their pieces alternately
-on a square board called a ‘chessboard’. The player with the white pieces commences the
-game. A player is said to ‘have the move’, when his opponent’s move has been ‘made’.
-"""
-
-def test_1_1_a():
-    'The game takes place on a chessboard'
-    chessboard = Chessboard()
-    print("[1.1.a][Pass]")
-def test_1_1_b():
-    'The player with the white pieces starts the game'
-    pass
-def test_1_1_c():
-    'A player "has the move" when his opponents move has been made'
-    pass
-    
-
-
 def main():
     print("===================")
     print("=== Chess Tests ===")
     print("===================")
     print("")
-
-    #Article 1 - The nature and objectives of the game of chess
-    #Article 1.1
-    test_1_1_a()
-    test_1_1_b()  
-    test_1_1_c()
 
     print("")
     print("-----------------------------------------")
