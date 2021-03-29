f228851d201bd492f29bac797c705f3e7ddace4b
elmartirta <elmartirta@rogers.com>
2021-03-29 16:02:08 -0400

    Remove PositionFactory, as it is not a Factory pattern.

--- Position.py
+++ Position.py
@@ -13,6 +13,53 @@ class Position():
         self.castlingRights = CastlingRights.fromAllTrue();
     def emptyEnpassantPawn():
         self.enPassantPawn = CartesianCoordinate.NONEXISTANT;
+    def addPiecesFromList(ANList):
+        pos = Position()
+        for pieceLocation in ANList:
+            pieceType = pieceLocation[0]
+            location = pieceLocation[1]
+            board.addPiece.(Coord.fromAN(location), pieceType)))
+        return pos
+    def fromStartingPosition():
+        pos = Position.fromEmpty();
+        return Position.addPiecesFromList([
+            ("a1","r"),
+            ("b1","n"),
+            ("c1","b"),
+            ("d1","q"),
+            ("e1","k"),
+            ("f1","b"),
+            ("g1","n"),
+            ("h1","r"),
+
+            ("a2", "p"),
+            ("b2", "p"),
+            ("c2", "p"),
+            ("d2", "p"),
+            ("e2", "p"),
+            ("f2", "p"),
+            ("g2", "p"),
+            ("h2", "p"),
+            
+            ("a7", "p"),
+            ("b7", "p"),
+            ("c7", "p"),
+            ("d7", "p"),
+            ("e7", "p"),
+            ("f7", "p"),
+            ("g7", "p"),
+            ("h7", "p"),
+            
+            ("a8","r"),
+            ("b8","n"),
+            ("c8","b"),
+            ("d8","q"),
+            ("e8","k"),
+            ("f8","b"),
+            ("g8","n"),
+            ("h8","r")
+        ]);
+
 
 class BoardState():
     def __init__(self):
