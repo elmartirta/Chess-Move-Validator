eb9125505f0aaf1d1f185a7f16c03969fc9eeb9c
elmartirta <elmartirta@rogers.com>
2021-04-06 21:26:35 -0400

    Add h8=Q style tests and pawn promotion generator

--- Move.py
+++ Move.py
@@ -2,7 +2,7 @@ import re
 from vector2D import Vector2D
 
 class Move():
-    def __init__(self, pieceType, sourceFile, sourceRank, destination, isCapture, isCheck, isCheckmate):
+    def __init__(self, pieceType, sourceFile, sourceRank, destination, isCapture, isCheck, isCheckmate, promotionPiece):
         self.pieceType = pieceType
         self.sourceFile = sourceFile
         self.sourceRank = int(sourceRank) if sourceRank else None
@@ -10,6 +10,7 @@ class Move():
         self.isCapture = isCapture 
         self.isCheck = isCheck
         self.isCheckmate = isCheckmate
+        self.promotionPiece = promotionPiece
     def clone(self):
         return Move(
             self.pieceType,
@@ -18,7 +19,8 @@ class Move():
             self.destination,
             self.isCapture,
             self.isCheck,
-            self.isCheckmate
+            self.isCheckmate,
+            self.promotionPiece
         )
     def color(self):
         return "black" if self.pieceType.isupper() else "white"
@@ -38,26 +40,43 @@ class Move():
         isCapture = "x" in string
         isCheck = "+" in string
         isCheckmate = "#" in string
+        promotionPiece = None
         
-        if re.fullmatch("[RNBQKP]x*[a-z]\d[+#]*", string):         #Parse Moves like Ke2, Be4+, Be4#
+        if re.fullmatch("[RNBQKP]x*[a-z]\d[+#]*", string):
+            #Parse Moves like Ke2, Be4+, Be4#
             destination = string.replace("x", "")[1:3]
-        elif re.fullmatch("[RNBQKP][a-z]x*[a-z]\d[+#]*", string):     #Parse moves like Rae4, etc
+        elif re.fullmatch("[RNBQKP][a-z]x*[a-z]\d[+#]*", string):
+            #Parse moves like Rae4, etc
             sourceFile = string[1]
             destination = string.replace("x", "")[2:4]
-        elif re.fullmatch("[RNBQKP]\dx*[a-z]\d[+#]*", string):     #Parse moves like R1e4, etc
+        elif re.fullmatch("[RNBQKP]\dx*[a-z]\d[+#]*", string):
+            #Parse moves like R1e4, etc
             sourceRank = string[1]
             destination = string.replace("x", "")[2:4]
-        elif re.fullmatch("[RNBQKP][a-z]\dx*[a-z]\d[+#]*", string):   #Parse moves like Qa1e4, etc
+        elif re.fullmatch("[RNBQKP][a-z]\dx*[a-z]\d[+#]*", string):
+            #Parse moves like Qa1e4, etc
             sourceFile = string[1]
             sourceRank = string[2]
             destination = string.replace("x", "")[3:5]
-        elif re.fullmatch("[a-z]\d[+#]*", string):         #Parse moves like e4, 
+        elif re.fullmatch("[a-z]\d[+#]*", string):
+            #Parse moves like e4, 
             pieceType = "P"
             destination = string[0:2]
-        elif re.fullmatch("[a-z]x[a-z]\d[+#]*", string):       #Parse moves like dxe4
+        elif re.fullmatch("[a-z]x[a-z]\d[+#]*", string):
+            #Parse moves like dxe4
             pieceType = "P"
             sourceFile = string[0]
             destination = string[2:4]
+        elif re.fullmatch("[a-z]8=[RNBQ][+#]*", string):
+            #Parse moves like a8=Q
+            pieceType = "P"
+            destination = string[0:2]
+            promotionPiece = string[3]
+        elif re.fullmatch("[a-z]x[a-z]8=[RNBQ][+#]*", string):
+            #Parse moves like dxa8=Q
+            pieceType = "P"
+            destination = string[2:4]
+            promotionPiece = string[5]
         elif re.fullmatch("o-o-o", string):
             raise MoveParsingError("Long Castling is not implemented yet.")
         elif re.fullmatch("o-o", string):
@@ -65,7 +84,7 @@ class Move():
         else:
             raise MoveParsingError("Move does not match any valid regex expression", string)
         destination = Vector2D.fromAN(destination)
-        return Move(pieceType, sourceFile, sourceRank, destination, isCapture, isCheck, isCheckmate)
+        return Move(pieceType, sourceFile, sourceRank, destination, isCapture, isCheck, isCheckmate, promotionPiece)
     def toString(self):
         return "Move : %s %s%s -> %s [Capt: %s, Check: %s, Mate: %s]" % (
             self.pieceType,
--- move_test_suite.py
+++ move_test_suite.py
@@ -6,7 +6,11 @@ class MoveTestSuite():
         return [
             {"runnable": MoveTestSuite.test1, "name": "Moves like : Ra1, Ra1+, Ra1#, Rxa1+"},
             {"runnable": MoveTestSuite.test2, "name": "Moves like : Rba1, Rba1+, etc"},
-            {"runnable": MoveTestSuite.test3, "name": "Moves like : R1a2, R1a2+, etc"}
+            {"runnable": MoveTestSuite.test3, "name": "Moves like : R1a2, R1a2+, etc"},
+            {"runnable": MoveTestSuite.test4, "name": "Moves like : Rb1a2, Rb1a2+, Rb1a2#, etc"},
+            {"runnable": MoveTestSuite.test5, "name": "Moves like : e4, e4#, e4+"},
+            {"runnable": MoveTestSuite.test6, "name": "Moves like : dxe4, dxe5#, dxe3+"},
+            {"runnable": MoveTestSuite.test7, "name": "Moves like : h8=Q, h8=Q+, h8=N#"}
         ]
     def test1():
         m = Move.fromAN("Ra1")
@@ -63,4 +67,80 @@ class MoveTestSuite():
         assert(m.isCapture == True)
         assert(m.isCheck == False)
         assert(m.isCheckmate == True)
+        return True
+    def test4():
+        m = Move.fromAN("Rb1a2")
+        assert(m.pieceType == "R" and m.destination == Vector2D.fromAN("a2") and m.sourceFile == "b" and m.sourceRank == 1)
+        assert(m.isCapture == False)
+        assert(m.isCheck == False)
+        assert(m.isCheckmate == False)
+        m = Move.fromAN("Ba1xh8#")
+        assert(m.pieceType == "B" and m.destination == Vector2D.fromAN("h8") and m.sourceFile == "a" and m.sourceRank == 1)
+        assert(m.isCapture == True)
+        assert(m.isCheck == False)
+        assert(m.isCheckmate == True)
+        m = Move.fromAN("Ba2h8+")
+        assert(m.pieceType == "B" and m.destination == Vector2D.fromAN("h8") and m.sourceFile == "a" and m.sourceRank == 2)
+        assert(m.isCapture == False)
+        assert(m.isCheck == True)
+        assert(m.isCheckmate == False)
+        return True
+    def test5():
+        m = Move.fromAN("e4")        
+        assert(m.pieceType == "P" and m.destination == Vector2D.fromAN("e4"))
+        assert(m.isCapture == False)
+        assert(m.isCheck == False)
+        assert(m.isCheckmate == False)
+        m = Move.fromAN("e4+")        
+        assert(m.pieceType == "P" and m.destination == Vector2D.fromAN("e4"))
+        assert(m.isCapture == False)
+        assert(m.isCheck == True)
+        assert(m.isCheckmate == False)
+        return True
+    def test6():
+        m = Move.fromAN("dxe4#")        
+        assert(m.pieceType == "P" and m.destination == Vector2D.fromAN("e4") and m.sourceFile == "d")
+        assert(m.isCapture == True)
+        assert(m.isCheck == False)
+        assert(m.isCheckmate == True)
+        return True
+    def test7():
+        m = Move.fromAN("h8=Q")
+        assert(m.pieceType == "P" and m.destination == Vector2D.fromAN("h8"))
+        assert(m.isCapture == False)
+        assert(m.isCheck == False)
+        assert(m.isCheckmate == False)
+        assert(m.promotionPiece == "Q")
+        m = Move.fromAN("a8=R+")
+        assert(m.pieceType == "P" and m.destination == Vector2D.fromAN("a8"))
+        assert(m.isCapture == False)
+        assert(m.isCheck == True)
+        assert(m.isCheckmate == False)
+        assert(m.promotionPiece == "R")
+        m = Move.fromAN("f8=N#")
+        assert(m.pieceType == "P" and m.destination == Vector2D.fromAN("f8"))
+        assert(m.isCapture == False)
+        assert(m.isCheck == False)
+        assert(m.isCheckmate == True)
+        assert(m.promotionPiece == "N")
+        return True
+    def test8():
+        m = Move.fromAN("bxc8=B")
+        assert(m.pieceType == "P" and m.destination == Vector2D.fromAN("c8") and m.sourceFile == "c")
+        assert(m.isCapture == False)
+        assert(m.isCheck == False)
+        assert(m.isCheckmate == False)
+        assert(m.promotionPiece == "B")
+        m = Move.fromAN("bxa8=Q+")
+        assert(m.pieceType == "P" and m.destination == Vector2D.fromAN("a8") and m.sourceFile == "b")
+        assert(m.isCapture == False)
+        assert(m.isCheck == True)
+        assert(m.isCheckmate == False)
+        assert(m.promotionPiece == "Q")
+        m = Move.fromAN("gxh8=R#")
+        assert(m.pieceType == "P" and m.destination == Vector2D.fromAN("h8") and m.sourceFile == "g")
+        assert(m.isCapture == False)
+        assert(m.isCheck == False)
+        assert(m.isCheckmate == True)
+        assert(m.promotionPiece == "R")
         return True
