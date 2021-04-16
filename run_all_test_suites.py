b929b58fa992ab5af065f54ec0896119bf86e296
elmartirta <elmartirta@rogers.com>
2021-04-16 14:15:28 -0400

    Add cool summary of all tests run to the test suites

--- run_all_test_suites.py
+++ run_all_test_suites.py
@@ -24,25 +24,34 @@ def runTests():
     allTestsPassed = True
     testsPassed = 0 
     testsRun = 0
+    summaryString = ""
     for suite in test_suites:
         test_number = 0
         print("-- %s ---:----->" % suite["suite_name"])
+        summaryString += suite["suite_name"][0]
         for test in suite["test_list"]:
             testsRun += 1
             test_number += 1
-            result = False
+            result = None
             try:
                 result = test["runnable"]()
                 if result == False:
                     allTestsPassed = False
+                    summaryString += (colorama.Fore.RED+"-"+colorama.Style.RESET_ALL)
                 else:
                     testsPassed += 1
+                    if result is None:
+                        summaryString += (colorama.Fore.CYAN+"u"+colorama.Style.RESET_ALL)
+                    else:
+                        summaryString += (colorama.Fore.GREEN+"+"+colorama.Style.RESET_ALL)
             except Exception as e:
                 allTestsPassed = False
                 print("["+colorama.Fore.YELLOW+"EXPT"+colorama.Style.RESET_ALL+"] %2d : !!!!!! Exception !!!!!!" % ( test_number))
                 print(colorama.Fore.YELLOW)
                 traceback.print_exc()
                 print(colorama.Style.RESET_ALL)
+                summaryString += colorama.Fore.YELLOW+"x"+colorama.Style.RESET_ALL
+
             finally:
                 print("[%s] %2d : %s" % (
                     colorama.Fore.GREEN+"PASS"+colorama.Style.RESET_ALL if result == True else colorama.Fore.RED+"FAIL"+colorama.Style.RESET_ALL if result == False else colorama.Fore.CYAN+"UNDF"+colorama.Style.RESET_ALL, 
@@ -51,6 +60,7 @@ def runTests():
                 )
     print(colorama.Fore.GREEN if allTestsPassed == True else colorama.Fore.RED)
     print("%d out of %d tests passed! %s\n (%d percent success rate)" % (testsPassed, testsRun, colorama.Style.RESET_ALL, (testsPassed * 100 / testsRun)))
+    print("[%s]" % summaryString)
     print()
     
 if __name__ == "__main__":
