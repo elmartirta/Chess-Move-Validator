afe62945b97e969ac70b251d4a6ca47fa275cf4f
elmartirta <elmartirta@rogers.com>
2021-04-22 13:12:17 -0400

    YAGNI - Remove historical code, and deprecate main()

--- Main.py
+++ Main.py
@@ -1,27 +1,5 @@
-from position import Position
-
-
 def main():
-  #Initialize Chessboard
-  chessboard = Position.fromStartingPosition()
-  chessboard.boardState.toString()
-  
-  #TODO: SMELL - Unused Commented Code
-  """
-  #Ask for tile selection
-  x = int(input("What is the X Vector of the piece you want to select?\n"))
-  y = int(input("What is the Y Vector of the piece you want to select?\n"))
-
-  #Ask for destination selection
-  newX = int(input("Where would you like to move the piece to (X-Vector)?"))
-  newY = int(input("Where would you like to move the piece to (Y-Vector)?"))
-  
-  chessboard.move(x, y, newX, newY)
-
-  #Print resultant board
-  print("Final Board State")
-  chessboard.draw()
-  """
+    raise NotImplementedError()
 
 if (__name__ == "__main__"):
-  main()
+    main()
