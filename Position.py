ee8d397118532c5f473ae77c03c314ff249213e3
elmartirta <elmartirta@rogers.com>
2021-05-03 13:42:51 -0400

    Remove code smell label of Chess960. It's fun, it stays.

--- Position.py
+++ Position.py
@@ -30,7 +30,7 @@ class Position():
             self.fullClock
         )
 
-    def fromChess960(seed=None): # TODO : SMELL - DEAD CODE
+    def fromChess960(seed=None):
         if seed: random.seed(seed)
         shuffled_pieces = "".join(random.sample("rnbkqbnr", k=8))
         return Position.fromFEN(
