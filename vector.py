d3bb871d6753ff4342d81d9b7c6701297fec3861
elmartirta <elmartirta@rogers.com>
2021-05-23 10:28:21 -0400

    Apply type hints to return values of Vector.py

--- vector.py
+++ vector.py
@@ -1,49 +1,53 @@
+from __future__ import annotations
+from typing import Iterable
+
+
 class Vector():
-    def __init__(self, x:int, y:int):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def __add__(self, other):
+    def __add__(self, other) -> Vector:
         if not isinstance(other, Vector): 
             raise TypeError(
                 "Vector addition must use vectors, not %s and %s"
                  % (str(self), str(other)))
         return Vector(self.x + other.x, self.y + other.y)
 
-    def __radd__(self, other):
+    def __radd__(self, other) -> Vector:
         if other == 0:
             return self
         else:
             return self.__add__(other)
 
-    def __sub__(self, other):
+    def __sub__(self, other) -> Vector:
         if not isinstance(other, Vector):
              raise TypeError("Vector subtraction must use vectors")
         return Vector(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, scalar):
+    def __mul__(self, scalar) -> Vector:
         if not isinstance(scalar, int):
             raise TypeError("Vector scalar multiplication must use an int")
         return Vector(self.x * scalar, self.y * scalar)
 
-    def __rmul__(self, scalar):
+    def __rmul__(self, scalar) -> Vector:
         return (self * scalar)
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         if self is None or other is None: return False
         if not isinstance(other, Vector): 
             raise TypeError("Vector equality must use vectors")
         return self.x == other.x and self.y == other.y
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "Vector(%s, %s)" % (str(self.x), str(self.y))
 
     @classmethod
-    def fromAN(cls, text):
+    def fromAN(cls, text) -> Vector:
         return cls.fromAlgebreicNotation(text)
 
     @classmethod
-    def fromAlgebreicNotation(cls, text):
+    def fromAlgebreicNotation(cls, text) -> Vector:
         toX = lambda chr : ord(chr.lower()) - 97
         toY = lambda chr : int(chr)-1
         if text is None or len(text) == 0:
@@ -59,36 +63,36 @@ class Vector():
             raise ANParsingError(text, "Length of the text is longer than 2")
         return cls(x,y)
 
-    def toAN(self):
+    def toAN(self) -> str:
         return self.toAlgebreicNotation()
 
-    def toAlgebreicNotation(self):
+    def toAlgebreicNotation(self) -> str:
         sourceRank = chr(self.x+97) if not self.x is None else ""
         sourceFile = str(self.y+1) if not self.y is None else ""
         return sourceRank + sourceFile
 
-    def plus(self, x, y):
+    def plus(self, x, y) -> Vector:
         return Vector(self.x + x, self.y + y)
 
-    def minus(self, x, y):
+    def minus(self, x, y) -> Vector:
         return Vector(self.x - x, self.y - y) 
 
-    def equals(self, x, y):
+    def equals(self, x, y) -> bool:
         return self == Vector(x,y)
 
-    def times(self, scalar):
+    def times(self, scalar) -> Vector:
         return self * scalar
 
-    def toString(self):
+    def toString(self) -> str:
         return str(self)
 
-    def isInsideChessboard(self):
+    def isInsideChessboard(self) -> bool:
         return self.x >= 0 and self.x <= 7 and self.y >= 0 and self.y <= 7
 
-    def between(self, other):
+    def between(self, other) -> Iterable[Vector]:
         return [self + delta for delta in (other - self).walk()]
 
-    def walk(self):
+    def walk(self) -> Iterable[Vector]:
         deltas = []
         direction = Vector(
             ((1 if self.x > 0 else -1) if self.x != 0 else 0),
@@ -102,7 +106,7 @@ class Vector():
             raise NotImplementedError("Walks are only defined in the diagonals and orthoganals.")
         return deltas
 
-    def clone(self):
+    def clone(self) -> Vector:
         return Vector(self.x, self.y)
 
         
