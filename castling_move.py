7d04134ff5af17374f4fe793ffa30f42f2384e51
elmartirta <elmartirta@rogers.com>
2021-06-04 10:57:21 -0400

    Define the UnfinishedCastlingMove class

--- castling_move.py
+++ castling_move.py
@@ -27,3 +27,37 @@ class CastlingMove():
         return self.source + (Vector(1,0) if self.isKingsideCastling else Vector(-1,0))
 
 
+@dataclass
+class UnfinishedCastlingMove():
+    pieceType: str
+    source: Optional[Vector]
+    destination: Optional[Vector]
+    isCapture: bool
+    isCheck: bool
+    isCheckmate: bool 
+    promotionPiece: Optional[Vector]
+    isKingsideCastling: bool
+    rookLocation: Optional[Vector]
+    
+    def clone(self):
+        return replace(self)
+
+    def __repr__(self) -> str:
+        return "Castling"+super().__repr__()
+
+    def midStep(self) -> Vector:
+        if self.source is None: raise ValueError
+        return self.source + (Vector(1,0) if self.isKingsideCastling else Vector(-1,0))
+    
+    def complete(self, source: Vector, destination: Vector, rookLocation: Vector) -> CastlingMove:
+        return CastlingMove(
+            self.pieceType,
+            source,
+            destination,
+            self.isCapture,
+            self.isCheck,
+            self.isCheckmate,
+            None,
+            self.isKingsideCastling,
+            rookLocation
+        )
