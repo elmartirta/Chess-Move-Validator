83b13e29c0b4668c84e6cfea2219de29d49d17cb
elmartirta <elmartirta@rogers.com>
2021-07-01 22:27:58 -0400

    Rename MoveFilter as Constraint

--- verifier/constraints.py
+++ verifier/constraints.py
@@ -3,105 +3,110 @@ from typing import Callable, List
 from .position import *
 from .castling_move import CastlingMove
 
-class MoveFilter():
+class Constraints():
     @staticmethod
-    def getPreMoveFilters() -> List[Callable[[Position, Move], FilterResult]]:
+    def getPreMoveConstraints() -> List[Callable[[Position, Move], ConstraintResult]]:
         return [
-            MoveFilter.checkSourcePiece,
-            MoveFilter.checkIfMoveWithinLegalBounds,
-            MoveFilter.checkIfDestinationIsOccupied,
-            MoveFilter.checkIfPathIsOccupied,
-            MoveFilter.checkIfCurrentKingInCheck
+            Constraints.sourcePieceExists,
+            Constraints.sourceTypeMatchesNotation,
+            Constraints.moveWithinLegalBounds,
+            Constraints.destinationUnoccupied,
+            Constraints.pathUnoccupied,
+            Constraints.currentKingNotInCheck
         ]
 
     @staticmethod
-    def getMidCastleFilters() -> List[Callable[[Position, Move], FilterResult]]:
-        return [MoveFilter.checkIfCurrentKingInCheck]
+    def getMidCastleConstraints() -> List[Callable[[Position, Move], ConstraintResult]]:
+        return [Constraints.currentKingNotInCheck]
 
     @staticmethod
-    def getPostMoveFilters() -> List[Callable[[Position, Move], FilterResult]]:
-        return [MoveFilter.checkIfOppositeKingInCheck]
+    def getPostMoveConstraints() -> List[Callable[[Position, Move], ConstraintResult]]:
+        return [Constraints.oppositeKingNotInCheck]
 
     @staticmethod
-    def checkSourcePiece(position: Position, move: Move) -> FilterResult:
+    def sourcePieceExists(_: Position, move: Move) -> ConstraintResult:
         if isinstance(move.source, UnfinishedVector):
-            raise FilterError(
+            raise ConstraintError(
                     """Which Piece? The Source file is not instantiated, 
                     leading to ambiguity.""",
-                move) 
-        elif position.board.pieceTypeOf(move.source) != move.pieceType:
-            raise FilterError(
+                move)
+        return ConstraintResult.accept(move)
+    
+    @staticmethod
+    def sourceTypeMatchesNotation(position: Position, move: Move) -> ConstraintResult:
+        if position.board.pieceTypeOf(move.source) != move.pieceType:
+            raise ConstraintError(
                     """Hustler's Piece: Type of source Piece 
                     (%s) does not match type of move piece (%s)."""
                     % (position.board.pieceTypeOf(move.source), move.pieceType),
                 move)
         elif position.board.pieceIsWhite(move.source) != position.isWhiteToMove:
-            return FilterResult.fail(
+            return ConstraintResult.fail(
                     """Wrong Color: The color of the move's 
                     source piece does not match the color that
                     is moving right now.""",
                 move) 
         else:
-            return FilterResult.accept(move)
+            return ConstraintResult.accept(move)
 
     @staticmethod
-    def checkIfMoveWithinLegalBounds(position: Position, move: Move) -> FilterResult:
+    def moveWithinLegalBounds(position: Position, move: Move) -> ConstraintResult:
         if not move.destination.isInsideChessboard():
-            return FilterResult.fail(
+            return ConstraintResult.fail(
                     """Out of Bounds: The move's destination is not 
                     within the legal bounds of an 8x8 chessboard""",
                 move)
         else:
-            return FilterResult.accept(move)
+            return ConstraintResult.accept(move)
  
     @staticmethod
-    def checkIfDestinationIsOccupied(position: Position, move: Move) -> FilterResult:
+    def destinationUnoccupied(position: Position, move: Move) -> ConstraintResult:
         if move.isCapture and (position.board.pieceIsWhite(move.destination) == position.isWhiteToMove):
-            return FilterResult.fail(
+            return ConstraintResult.fail(
                     """Friendly Fire: The move involves a piece trying to 
                     capture a target of the same color.""",
                 move)
         elif move.isCapture and position.board.isEmptyAt(move.destination):
-            return FilterResult.fail(
+            return ConstraintResult.fail(
                     """Starved Attacker: Move is a capture, but there is no 
                     piece to capture on the destination square""",
                 move)
         elif not move.isCapture and not position.board.isEmptyAt(move.destination):
-            return FilterResult.fail(
+            return ConstraintResult.fail(
                     """Cramped Quarters: The Move is not a capture,
                     and the destination is not empty""",
                 move)
         elif isinstance(move, CastlingMove):
             if not position.board.isEmptyAt(move.midStep()):
-                return FilterResult.fail(
+                return ConstraintResult.fail(
                         """Castling Blocked: There is a piece between the 
                         king's source and destination squares.""",
                     move) 
-        return FilterResult.accept(move)
+        return ConstraintResult.accept(move)
     
     @staticmethod
-    def checkIfPathIsOccupied(position: Position, move: Move) -> FilterResult:
+    def pathUnoccupied(position: Position, move: Move) -> ConstraintResult:
         if move.pieceType in "N":
-            return FilterResult.accept(move)
+            return ConstraintResult.accept(move)
         if not position.board.pieceCanSee(move.source, move.destination):
-            return FilterResult.fail(
+            return ConstraintResult.fail(
                     """Obstructed: A piece is in the way of the move.""",
                 move)
-        return FilterResult.accept(move)
+        return ConstraintResult.accept(move)
 
     @staticmethod
-    def checkIfCurrentKingInCheck(position: Position, move: Move) -> FilterResult:
-        return MoveFilter._checkIfKingInCheck(position.board, move, position.isWhiteToMove)
+    def currentKingNotInCheck(position: Position, move: Move) -> ConstraintResult:
+        return Constraints._checkIfKingInCheck(position.board, move, position.isWhiteToMove)
 
     @staticmethod
-    def checkIfOppositeKingInCheck(position: Position, move: Move) -> FilterResult:
-        return MoveFilter._checkIfKingInCheck(position.board, move, not position.isWhiteToMove)
+    def oppositeKingNotInCheck(position: Position, move: Move) -> ConstraintResult:
+        return Constraints._checkIfKingInCheck(position.board, move, not position.isWhiteToMove)
         
     @staticmethod
-    def _checkIfKingInCheck(board: Board, move: Move, kingIsWhite: bool) -> FilterResult:
+    def _checkIfKingInCheck(board: Board, move: Move, kingIsWhite: bool) -> ConstraintResult:
         kingSymbol = "K" if kingIsWhite else "k"
         if len(board.findAll(kingSymbol)) == 0:
-            return FilterResult.fail(
+            return ConstraintResult.fail(
                     """There is no king of the right color on the board""",
                 move)
         kingLocations = board.findAll(kingSymbol)
@@ -113,7 +118,7 @@ class MoveFilter():
                         and (
                             board.pieceTypeOf(candidate) not in "RBQ" \
                             or board.pieceCanSee(candidate, king)):
-                    return FilterResult.fail(
+                    return ConstraintResult.fail(
                             "The king on %s is being checked by the piece on %s"
                             % (king.toAN(), candidate.toAN()),
                         move)
@@ -129,27 +134,27 @@ class MoveFilter():
         if error: 
             return error
         else:
-            return FilterResult.accept(move)  
+            return ConstraintResult.accept(move)  
 
 @dataclass
-class FilterResult():
+class ConstraintResult():
     reason: str
     move: Move
     isLegal: bool = True
 
     @staticmethod
-    def accept(move: Move) -> FilterResult:
-        return FilterResult("", move, True)
+    def accept(move: Move) -> ConstraintResult:
+        return ConstraintResult("", move, True)
 
     @staticmethod
-    def fail(reason: str, move: Move) -> FilterResult:
+    def fail(reason: str, move: Move) -> ConstraintResult:
         assert(isinstance(reason, str))
-        return FilterResult(
+        return ConstraintResult(
                 f"The move {move} fails the filtration process because: {reason}",
             move, False)
 
 
-class FilterError(ValueError):
+class ConstraintError(ValueError):
     def __init__(self, reason: str, move: Move):
         super().__init__(
             f"The move {move} is unable to be properly filtered, because: {reason}")
--- verifier/move_verifier.py
+++ verifier/move_verifier.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typing import List, Union
 from .move_generator import MoveGenerator
-from .move_filter import MoveFilter
+from .constraints import Constraints
 from .position import Position
 from .move import Move, UnfinishedMove
 from .castling_move import CastlingMove
@@ -9,7 +9,7 @@ from .castling_move import CastlingMove
 
 class MoveVerifier():
     @staticmethod
-    def verifyGame(position: Position, moveList: List[UnfinishedMove]) -> VerificationResult:
+    def verifyGame(position: Position, moveList: List[UnfinishedMove]) -> ConstraintResult:
         currentPosition = position
         for move in moveList:
             result = MoveVerifier.verifyMove(currentPosition, move)
@@ -19,61 +19,60 @@ class MoveVerifier():
         return result
 
     @staticmethod
-    def verifyMove(position: Position, move: UnfinishedMove) -> VerificationResult:
-        filteredMoves: List[VerificationResult] = [] #TODO: SMELL - Misrepresentative Name
+    def verifyMove(position: Position, move: UnfinishedMove) -> ConstraintResult:
+        constrainedMoves: List[ConstraintResult] = [] #TODO: SMELL - Misrepresentative Name
         for candidate in MoveGenerator.generateMoveList(position, move):
-            filteredMoves.append(
+            constrainedMoves.append(
                 MoveVerifier.verifyCandidate(position, candidate)
             )
-        legalMoves: List[VerificationResult] = [m for m in filteredMoves if m.isLegal] #TODO: SMELL - Misrepresentative Name
-        if len(legalMoves) == 0 and len(filteredMoves) == 1:
-            return VerificationResult.fail(
+        legalMoves: List[ConstraintResult] = [m for m in constrainedMoves if m.isLegal] #TODO: SMELL - Misrepresentative Name
+        if len(legalMoves) == 0 and len(constrainedMoves) == 1:
+            return ConstraintResult.fail(
                     "Illegal Move: The move %s is illegal because %s"
-                    % (move, filteredMoves[0].reason),
+                    % (move, constrainedMoves[0].reason),
                 position, move)
         elif len(legalMoves) == 0: 
-            return VerificationResult.fail(
+            return ConstraintResult.fail(
                     "No Legal Moves: The move %s is illegal because %s"
-                    % (move, [result.reason for result in filteredMoves]),
+                    % (move, [result.reason for result in constrainedMoves]),
                 position, move)
         elif len(legalMoves) > 1:
-
-            return VerificationResult.fail(
+            return ConstraintResult.fail(
                     "Ambiguous Move: The move %s leads to multiple valid moves [%s]"
                     % (move, legalMoves),
                 position, move)
         elif len(legalMoves) == 1:
             validMove = legalMoves[0] #TODO: SMELL - Mysterious Name
-            return VerificationResult.accept(validMove.updatedPosition, validMove.move) 
+            return ConstraintResult.accept(validMove.updatedPosition, validMove.move) 
         else:
             raise NotImplementedError("Impossible Code")
     
     @staticmethod
-    def verifyCandidate(position: Position, move: Move) -> VerificationResult:
-        def checkAllFilters(filters, position, move):
-            for moveFilter in filters:
-                filterResult = moveFilter(position, move)
-                if not filterResult.isLegal:
-                    return VerificationResult.fail(filterResult.reason, position, move)
+    def verifyCandidate(position: Position, move: Move) -> ConstraintResult:
+        def checkConstraints(constraints, position, move):
+            for constraint in constraints:
+                result = constraint(position, move)
+                if not result.isLegal:
+                    return ConstraintResult.fail(result.reason, position, move)
 
-        checkAllFilters(MoveFilter.getPreMoveFilters(), position, move)
+        checkConstraints(Constraints.getPreMoveConstraints(), position, move)
 
         if isinstance(move, CastlingMove):
             halfPosition = position.halfCastle(move)
-            checkAllFilters(MoveFilter.getMidCastleFilters(), position, move)
+            checkConstraints(Constraints.getMidCastleConstraints(), position, move)
             newPosition = halfPosition.finishCastle(move)
         else:
             newPosition = position.next(move)
 
-        for moveFilter in MoveFilter.getPostMoveFilters():
-            filterResult = moveFilter(newPosition, move)
-            if not filterResult.isLegal:
-                return VerificationResult.fail(filterResult.reason, newPosition, move)
+        for constraint in Constraints.getPostMoveConstraints():
+            result = constraint(newPosition, move)
+            if not result.isLegal:
+                return ConstraintResult.fail(result.reason, newPosition, move)
 
-        return VerificationResult.accept(newPosition, move)
+        return ConstraintResult.accept(newPosition, move)
 
 
-class VerificationResult():
+class ConstraintResult():
     def __init__(self, reason: str, updatedPosition: Position, move: Union[UnfinishedMove | Move], isLegal: bool):
         self.reason = reason
         self.updatedPosition = updatedPosition
@@ -81,10 +80,10 @@ class VerificationResult():
         self.isLegal = isLegal
 
     @staticmethod
-    def accept(position: Position, move: Union[UnfinishedMove | Move]) -> VerificationResult:
-        return VerificationResult("", position, move, True)
+    def accept(position: Position, move: Union[UnfinishedMove | Move]) -> ConstraintResult:
+        return ConstraintResult("", position, move, True)
 
     @staticmethod
-    def fail(reason: str, position: Position, move: Union[UnfinishedMove | Move]) -> VerificationResult:
+    def fail(reason: str, position: Position, move: Union[UnfinishedMove | Move]) -> ConstraintResult:
         assert(isinstance(reason, str))
-        return VerificationResult(reason, position, move, False)
+        return ConstraintResult(reason, position, move, False)
